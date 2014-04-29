d180fe666f001c642ffe7b93df3597791214f51d
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-29 20:21:07 +0200

    attempts to get one wave only

--- matlabfiles/advectStart.m
+++ matlabfiles/advectStart.m
@@ -1,5 +1,9 @@
 %% advect linearization
 clear all;
+clc;
+close all;
+addpath(sprintf('/Users/kevin/SkyDrive/KTH Work/LaTeX Reports',...
+'/HW4-High Resolution shock-capturing methods/matlabfiles'));
 
 %% Parameters
 N = 200;
@@ -10,13 +14,13 @@ g = 9.8;
 c = g; % Wave speed
 tau = .5*dx/g; % Time Step
 coeff = -tau/(2*dx);
-nStep = L/(c*tau);
+nStep = 10*L/(c*tau);
 i = 1:N;
 %% Boundary Conditions
-w = 0.4; % Width
+w = 0.1*L; % Width
 x = (i-1/2)*dx; % Grid points
-eps = 0.1;
-h = H + eps*exp(-(x-L/2).^2/(w^2));
+a = 1/5*H;
+h = H + a*exp(-x.^2/(w^2));
 m = zeros(1,N);
 % Reflection Boundary Conditions
 ip = i+1;
@@ -26,13 +30,13 @@ im(1) = N;
 %% Record the initial state
 hplot(:,1) = h(:);
 mplot(:,1) = m(:);
-flux = (1/2*g*h.^2 + (m.^2)./h.^2);
+flux = (1/2*g*h.^2 + (m.^2)./h);
 %% Loop
-for iStep=1:round(nStep)
+for iStep=1:(round(nStep))
     hOld=h;
     mOld=m;
+    flux = (1/2*g*hOld.^2 + (mOld.^2)./hOld);
     h(1:N) = .5*(hOld(ip)+hOld(im)) + coeff*(mOld(ip)-mOld(im));
-    flux = (1/2*g*h.^2 + (mOld.^2)./h);
     m(1:N) = .5*(mOld(ip)+mOld(im)) + coeff*(flux(ip)-flux(im));
     hplot(:,iStep) = h(:);
     mplot(:,iStep) = m(:);
@@ -43,11 +47,10 @@ figure(1); clf;
 plot(x,hplot(:,1),'-',x,h,'--');
 legend('t=0  ','t=1');
 xlabel('x');  ylabel('h(x,t)');
-title(sprintf('$h(x,t)$ $\\Delta t=$%0.2g, $\\epsilon =$ %g',tau,eps),...
+title(sprintf('$h(x,t)$ $\\Delta t=$%0.2g, $\\epsilon =$ %g',tau,a),...
     'Interpreter','latex')
-%%
+%% h plot
 figure(2)
-
 for ip = 1:8:nStep
     clf;
     plot(x,hplot(:,1))
@@ -55,8 +58,17 @@ for ip = 1:8:nStep
     plot(x,hplot(:,ip),'-');
     pause(.01)
 end
+%% m plot
+% figure(3)
+% for ip = 1:8:nStep
+%     clf;
+%     plot(x,mplot(:,1))
+%     hold on;
+%     plot(x,mplot(:,ip),'-');
+%     pause(.01)
+% end
 %%
-printYesNo = 1;
+printYesNo = 0;
 if printYesNo == 1
     saveFigurePath = '/Users/kevin/SkyDrive/KTH Work/LaTeX Reports/Stability HW3/Figures/';
     set(figure(1), 'PaperPositionMode', 'auto');
