4dd1d4c9134abbf5828391ddc714f1dd83a432a5
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-30 03:27:23 +0200

    Ghost Cells corrected
    Still need to make it independent of number of ghost cells…current
    amount is 2 ghost cells

--- matlabfiles/advectStart.m
+++ matlabfiles/advectStart.m
@@ -3,10 +3,10 @@ clear all;
 clc;
 close all;
 addpath(['/Users/kevin/SkyDrive/KTH Work/LaTeX Reports',...
-'/HW4-High Resolution shock-capturing methods/matlabfiles/']);
+    '/HW4-High Resolution shock-capturing methods/matlabfiles/']);
 
 %% Parameters
-N = 10;
+N = 100;
 L = 1.;
 dx = L/N; % Grid spacing
 H = 1;
@@ -14,28 +14,32 @@ g = 9.8;
 c = g; % Wave speed
 tau = .5*dx/g; % Time Step
 coeff = -tau/(2*dx);
-nStep = L/(c*tau);
-i = 1:N;
+nStep = 2*L/(c*tau);
+nCells = 1:N;
 ghostCellOneSide = 2;
+cellsPlusGhost = 1:(N+2*ghostCellOneSide);
 %% Boundary Conditions
 w = 0.1*L; % Width
-x = (i-1/2)*dx; % Grid points
+x = (nCells-1/2)*dx; % Grid points
 a = 1/5*H;
 h = H + a*exp(-(x-L/2).^2/(w^2));
-m = zeros(1,N + ghostCellOneSide*2);
 % Boundary Conditions
 h = padarray(h',ghostCellOneSide)';
-%% BC 
+m = zeros(1,length(h));
+%% BC
 % needs to be changed if ghostCellOneSide is changed
 h(1) = h(4);
 h(2) = h(3);
 m(1) = m(4)*(-1);
 m(2) = m(3)*(-1);
-h(N+2) = h(N-1);
-h(N+1) = h(N);
-m(N+2) = m(N-1)*(-1);
-m(N+1) = m(N)*(-1);
-ip = 
+h(N+4) = h(N+1);
+h(N+3) = h(N+2);
+m(N+4) = m(N+1)*(-1);
+m(N+3) = m(N+2)*(-1);
+%%
+iWithGhost = ((1+ghostCellOneSide):(N+ghostCellOneSide));
+ip = iWithGhost+1;
+im = iWithGhost-1;
 %% Record the initial state
 hplot(:,1) = h(:);
 mplot(:,1) = m(:);
@@ -45,28 +49,36 @@ for iStep=1:(round(nStep))
     hOld=h;
     mOld=m;
     flux = (1/2*g*hOld.^2 + (mOld.^2)./hOld);
-    h(1:N) = .5*(hOld(ip)+hOld(i)) + coeff*(mOld(ip)-mOld(i));
-    m(1:N) = .5*(mOld(ip)+mOld(i)) + coeff*(flux(ip)-flux(i));
+    h(3:(N+2)) = .5*(hOld(ip)+hOld(im)) + coeff*(mOld(ip)-mOld(iWithGhost));
+    m(3:(N+2)) = .5*(mOld(ip)+mOld(im)) + coeff*(flux(ip)-flux(iWithGhost));
+    h(1) = h(4);
+    h(2) = h(3);
+    m(1) = m(4)*(-1);
+    m(2) = m(3)*(-1);
+    h(N+4) = h(N+1);
+    h(N+3) = h(N+2);
+    m(N+4) = m(N+1)*(-1);
+    m(N+3) = m(N+2)*(-1);
     hplot(:,iStep) = h(:);
     mplot(:,iStep) = m(:);
 end
 
 %% Plot
-figure(1); clf;
-plot(x,hplot(:,1),'-',x,h,'--');
-legend('t=0  ','t=1');
-xlabel('x');  ylabel('h(x,t)');
-title(sprintf('$h(x,t)$ $\\Delta t=$%0.2g,',...
-    ' $\\epsilon =$ %g',tau,a),...
-    'Interpreter','latex')
+% figure(1); clf;
+% plot(x,hplot(3:(N+2),1),'-',x,h,'--');
+% legend('t=0  ','t=1');
+% xlabel('x');  ylabel('h(x,t)');
+% title(sprintf('$h(x,t)$ $\\Delta t=$%0.2g,',...
+%     ' $\\epsilon =$ %g',tau,a),...
+%     'Interpreter','latex')
 %% h plot
 figure(2)
-for ip = 1:8:nStep
+for ip = 1:2:nStep
     clf;
-    plot(x,hplot(:,1))
+    plot(x,hplot((3:(N+2)),1))
     hold on;
-    plot(x,hplot(:,ip),'-');
-    pause(.01)
+    plot(x,hplot((3:(N+2)),ip),'-');
+    pause(.1)
 end
 %% m plot
 % figure(3)
